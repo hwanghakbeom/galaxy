e59d7d5d4cd51cada59fe6896fed2759a8240942
AndyHwang <hwanghakbeom@gmail.com>
2013-12-03 17:00:17 +0900

    chnage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :posts
+  map.resources :layouts
 
   # The priority is based upon order of creation: first created -> highest priority.
 
